f2627abaedda3b0e371f380edcc84d67c10a492c
red <red_program@163.com>
2017-07-20 15:49:53 +0800

    插入排序

---
+++ LearnAlgorithm/InsertionSort.hpp
@@ -0,0 +1,31 @@
+﻿// Created by Buwenjie
+// 插入排序
+#pragma once
+
+#include <stdio.h>
+#include <vector>
+
+void InsertionSort(std::vector<int>& data) {
+	for (int i = 1; i < data.size(); i++) {
+		// 当前要插入的值v
+		int v = data[i];
+		// 从位置i-1向前遍历，大于v的后移一个位置
+		int j = i - 1;
+		while (j >= 0 && data[j] > v) {
+			data[j + 1] = data[j];
+			j -= 1;
+		}
+		// v插入到空出的位置中
+		data[j + 1] = v;
+	}
+}
+
+
+void test_InsertionSort() {
+	std::vector<int> data = { 3, 1, 5, 7, 6, 2, 9, 4, 8 };
+	InsertionSort(data);
+	for (int v : data) {
+		printf("%d ", v);
+	}
+	printf("\n");
+}
