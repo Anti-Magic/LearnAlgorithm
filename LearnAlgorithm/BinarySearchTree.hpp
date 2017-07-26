b0774a4afd3e398aa6806875b6f2e880f3966bac
red <red_program@163.com>
2017-07-26 15:14:24 +0800

    二叉搜索树：插入，中序遍历

---
+++ LearnAlgorithm/BinarySearchTree.hpp
@@ -0,0 +1,96 @@
+﻿// Created by Buwenjie
+#pragma once
+
+#include <vector>
+
+class BinarySearchTreeNode {
+public:
+	BinarySearchTreeNode(int value)
+	: mValue(value)
+	, mLeft(nullptr)
+	, mRight(nullptr) {
+	}
+	int mValue;
+	BinarySearchTreeNode* mLeft;
+	BinarySearchTreeNode* mRight;
+};
+
+class BinarySearchTree {
+public:
+	BinarySearchTree()
+	: mRoot(nullptr) {
+	}
+
+	~BinarySearchTree() {
+		freeNode(mRoot);
+	}
+
+	void insert(int value) {
+		if (mRoot == nullptr) {
+			mRoot = new BinarySearchTreeNode(value);
+		}
+		else {
+			BinarySearchTreeNode* p = mRoot;
+			while (true) {
+				if (value <= p->mValue) {
+					if (p->mLeft == nullptr) {
+						p->mLeft = new BinarySearchTreeNode(value);
+						break;
+					}
+					else {
+						p = p->mLeft;
+					}
+				}
+				else {
+					if (p->mRight == nullptr) {
+						p->mRight = new BinarySearchTreeNode(value);
+						break;
+					}
+					else {
+						p = p->mRight;
+					}
+				}
+			}
+		}
+	}
+
+	void inorderWalk(std::vector<int>& v) {
+		inorderWalkNode(mRoot, v);
+	}
+
+private:
+	void inorderWalkNode(BinarySearchTreeNode* node, std::vector<int>& v) {
+		if (node == nullptr) {
+			return;
+		}
+		inorderWalkNode(node->mLeft, v);
+		v.push_back(node->mValue);
+		inorderWalkNode(node->mRight, v);
+	}
+
+	void freeNode(BinarySearchTreeNode* node) {
+		if (node == nullptr) {
+			return;
+		}
+		freeNode(node->mLeft);
+		freeNode(node->mRight);
+		delete node;
+	}
+
+	BinarySearchTreeNode* mRoot;
+};
+
+#include <stdio.h>
+void test_BinarySearchTree() {
+	std::vector<int> data = { 3, 1, 5, 7, 6, 2, 9, 4, 8 };
+	BinarySearchTree tree;
+	for (auto v : data) {
+		tree.insert(v);
+	}
+	std::vector<int> res;
+	tree.inorderWalk(res);
+	for (auto v : res) {
+		printf("%d ", v);
+	}
+	printf("\n");
+}
